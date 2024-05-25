3ba4ed069a33a5046880c6d8cad9183105ef67d6
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-05-25 12:11:51 +0930

    saaa

---
+++ src/app/page.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Box, Button, HStack, VStack } from "@chakra-ui/react";
+const page = () => {
+  return (
+    <>
+      <HStack>
+        <Box w={"100%"} h={"100%"} mt={"20px"}>
+          <HStack justify={"center"} w={"100%"}>
+            <HStack
+              justify={"space-between"}
+              w={["90%", "80%", "90%", "70%", "50%"]}
+              rounded={"xl"}
+              px={["3", "3", "10", "20", "20"]}
+            >
+              <Button
+                variant={"none"}
+                color={"black"}
+                fontSize={["10px", "13px", "14px", "14px", "15px"]}
+              >
+                Home
+              </Button>
+
+              <Button
+                variant={"none"}
+                color={"black"}
+                fontSize={["10px", "11px", "14px", "14px", "15px"]}
+              >
+                Features
+              </Button>
+
+              <Button
+                variant={"none"}
+                color={"black"}
+                fontSize={["10px", "11px", "14px", "14px", "15px"]}
+              >
+                Roadmap
+              </Button>
+
+              <Button
+                variant={"none"}
+                color={"black"}
+                fontSize={["10px", "11px", "14px", "14px", "15px"]}
+              >
+                Contact
+              </Button>
+            </HStack>
+          </HStack>
+        </Box>
+      </HStack>
+    </>
+  );
+};
+
+export default page;
