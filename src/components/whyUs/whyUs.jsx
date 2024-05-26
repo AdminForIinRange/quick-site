d14467390046ddfd64f29878eb333bb6255566fb
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-05-26 12:06:39 +0930

    import React from 'react'
    const Service = () => {
      return (
        <>Service</>
      )
    }
    
    export default Service

--- src/components/whyUs/whyUs.jsx
+++ src/components/whyUs/whyUs.jsx
@@ -10,133 +10,118 @@ const WhyUs = () => {
     </HStack>
     <HStack justify={"center"} w={"100%"} h={"100%"} mt={"25px"}
     gap={["25px", "100px", "200px", "350px", "450px"]} >
-      <VStack>
-      <Text
-          color={"#303030"}
-          fontSize={["50px", "50px", "60px", "80px", "95px"]}
-          fontFamily={"Playfair Display"}
-          fontWeight={"600"}
-          textAlign={"start"}>
-         500+
-          </Text>
-          <Text
-          color={"#303030"}
-          fontSize={"16px"}
-          fontFamily={"Playfair Display"}
-          fontWeight={"300"}
-          textAlign={"center"}  w={"80%"}   >
-       Renovations Completed
-          </Text>
-        
-      </VStack>
+   
 
-      <VStack>
-      <Text
-          color={"#303030"}
-          fontSize={["50px", "50px", "60px", "80px", "95px"]}
-          fontFamily={"Playfair Display"}
-          fontWeight={"600"}
-          textAlign={"start"}>
-        97%
-          </Text>
-          <Text
-          color={"#303030"}
-          fontSize={"16px"}
-          fontFamily={"Playfair Display"}
-          fontWeight={"300"}
-          textAlign={"center"}  w={"80%"}  >
-      Customer Satisfaction Rate
-          </Text>
-        
-      </VStack>
+  <VStack justify={"center"} w={"100%"} h={"100%"} align={"center"} gap={"20px"}> 
 
-    </HStack>
-    <HStack justify={"center"} w={"100%"} h={"100%"} mt={"25px"}
-   gap={["25px", "100px", "200px", "350px", "450px"]}>
-      <VStack>
-      <Text
-          color={"#303030"}
-          fontSize={["50px", "50px", "60px", "80px", "95px"]}
-          fontFamily={"Playfair Display"}
-          fontWeight={"600"}
-          textAlign={"start"}>
-        200
-          </Text>
-          <Text
-          color={"#303030"}
-          fontSize={"16px"}
-          fontFamily={"Playfair Display"}
-          fontWeight={"300"}
-          textAlign={"center"}  w={"80%"}  >
-      Design Awards Won
-          </Text>
-        
-      </VStack>
+  <Text
+    color={"#303030"}
+    fontSize={["50px", "50px", "60px", "80px", "95px"]}
+    fontFamily={"Playfair Display"}
+    fontWeight={"600"}
+    textAlign={"start"}>
+   500+
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={"16px"}
+    fontFamily={"Playfair Display"}
+    fontWeight={"300"}
+    textAlign={"center"}  w={"80%"}   >
+ Renovations Completed
+    </Text>
 
-      <VStack>
-      <Text
-          color={"#303030"}
-          fontSize={["50px", "50px", "60px", "80px", "95px"]}
-          fontFamily={"Playfair Display"}
-          fontWeight={"600"}
-          textAlign={"start"}>
-        300
-          </Text>
-          <Text
-          color={"#303030"}
-          fontSize={"16px"}
-          fontFamily={"Playfair Display"}
-          fontWeight={"300"}
-          textAlign={"center"}  w={"80%"}  >
-      Custom Projects Per Year
-          </Text>
-        
-      </VStack>
+    <Text
+    color={"#303030"}
+    fontSize={["50px", "50px", "60px", "80px", "95px"]}
+    fontFamily={"Playfair Display"}
+    fontWeight={"600"}
+    textAlign={"start"}>
+200
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={"16px"}
+    fontFamily={"Playfair Display"}
+    fontWeight={"300"}
+    textAlign={"center"}  w={"80%"}   >
+Design Awards Won
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={["50px", "50px", "60px", "80px", "95px"]}
+    fontFamily={"Playfair Display"}
+    fontWeight={"600"}
+    textAlign={"start"}>
+25
 
-    </HStack>
-  
-    <HStack justify={"center"} w={"100%"} h={"100%"} mt={"25px"}
-  gap={["25px", "100px", "200px", "350px", "450px"]} >
-      <VStack>
-      <Text
-          color={"#303030"}
-          fontSize={["50px", "50px", "60px", "80px", "95px"]}
-          fontFamily={"Playfair Display"}
-          fontWeight={"600"}
-          textAlign={"start"}>
-        25
-          </Text>
-          <Text
-          color={"#303030"}
-          fontSize={"16px"}
-          fontFamily={"Playfair Display"}
-          fontWeight={"300"}
-          textAlign={"center"}  w={"80%"}  >
-     Years of Industry Experience
-          </Text>
-        
-      </VStack>
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={"16px"}
+    fontFamily={"Playfair Display"}
+    fontWeight={"300"}
+    textAlign={"center"}  w={"80%"}   >
+Years of Industry Experience
+    </Text>
 
-      <VStack>
-      <Text
-          color={"#303030"}
-          fontSize={["50px", "50px", "60px", "80px", "95px"]}
-          fontFamily={"Playfair Display"}
-          fontWeight={"600"}
-         textAlign={"center"}>
-      5
-          </Text>
-          <Text
-          color={"#303030"}
-          fontSize={"16px"}
-          fontFamily={"Playfair Display"}
-          fontWeight={"300"}
-          textAlign={"center"}  w={"80%"}  >
-     Offices Nationwide
-          </Text>
-        
-      </VStack>
+  </VStack>
 
+
+
+  <VStack justify={"center"} w={"100%"} h={"100%"} align={"center"} gap={"20px"}> 
+
+  <Text
+    color={"#303030"}
+    fontSize={["50px", "50px", "60px", "80px", "95px"]}
+    fontFamily={"Playfair Display"}
+    fontWeight={"600"}
+    textAlign={"start"}>
+97%
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={"16px"}
+    fontFamily={"Playfair Display"}
+    fontWeight={"300"}
+    textAlign={"center"}  w={"80%"}   >
+Customer Satisfaction Rate
+    </Text>
+
+    <Text
+    color={"#303030"}
+    fontSize={["50px", "50px", "60px", "80px", "95px"]}
+    fontFamily={"Playfair Display"}
+    fontWeight={"600"}
+    textAlign={"start"}>
+300
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={"16px"}
+    fontFamily={"Playfair Display"}
+    fontWeight={"300"}
+    textAlign={"center"}  w={"80%"}   >
+Custom Projects Per Year
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={["50px", "50px", "60px", "80px", "95px"]}
+    fontFamily={"Playfair Display"}
+    fontWeight={"600"}
+    textAlign={"start"}>
+5
+    </Text>
+    <Text
+    color={"#303030"}
+    fontSize={"16px"}
+    fontFamily={"Playfair Display"}
+    fontWeight={"300"}
+    textAlign={"center"}  w={"80%"}   >
+Offices Nationwide
+    </Text>
+
+  </VStack>
     </HStack>
   
   
@@ -146,3 +131,4 @@ const WhyUs = () => {
 }
 
 export default WhyUs
+
