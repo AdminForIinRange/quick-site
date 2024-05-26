ef6a4116b1afe337f088abb48aa40d317113be16
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-05-26 11:35:30 +0930

    minor chnages

--- src/components/whyUs/whyUs.jsx
+++ src/components/whyUs/whyUs.jsx
@@ -1,8 +1,147 @@
 import React from 'react'
-
+import { Box, HStack, Text, VStack } from "@chakra-ui/react"
 const WhyUs = () => {
   return (
-    <>WhyUs</>
+    <>
+    <HStack justify={"center"} w={"100%"} h={"100%"}  mt={"150px"}  >
+
+
+     
+    </HStack>
+    <HStack justify={"center"} w={"100%"} h={"100%"} mt={"25px"}
+    gap={["25px", "100px", "200px", "350px", "450px"]} >
+      <VStack>
+      <Text
+          color={"#303030"}
+          fontSize={["50px", "50px", "60px", "80px", "95px"]}
+          fontFamily={"Playfair Display"}
+          fontWeight={"600"}
+          textAlign={"start"}>
+         500+
+          </Text>
+          <Text
+          color={"#303030"}
+          fontSize={"16px"}
+          fontFamily={"Playfair Display"}
+          fontWeight={"300"}
+          textAlign={"center"}  w={"80%"}   >
+       Renovations Completed
+          </Text>
+        
+      </VStack>
+
+      <VStack>
+      <Text
+          color={"#303030"}
+          fontSize={["50px", "50px", "60px", "80px", "95px"]}
+          fontFamily={"Playfair Display"}
+          fontWeight={"600"}
+          textAlign={"start"}>
+        97%
+          </Text>
+          <Text
+          color={"#303030"}
+          fontSize={"16px"}
+          fontFamily={"Playfair Display"}
+          fontWeight={"300"}
+          textAlign={"center"}  w={"80%"}  >
+      Customer Satisfaction Rate
+          </Text>
+        
+      </VStack>
+
+    </HStack>
+    <HStack justify={"center"} w={"100%"} h={"100%"} mt={"25px"}
+   gap={["25px", "100px", "200px", "350px", "450px"]}>
+      <VStack>
+      <Text
+          color={"#303030"}
+          fontSize={["50px", "50px", "60px", "80px", "95px"]}
+          fontFamily={"Playfair Display"}
+          fontWeight={"600"}
+          textAlign={"start"}>
+        200
+          </Text>
+          <Text
+          color={"#303030"}
+          fontSize={"16px"}
+          fontFamily={"Playfair Display"}
+          fontWeight={"300"}
+          textAlign={"center"}  w={"80%"}  >
+      Design Awards Won
+          </Text>
+        
+      </VStack>
+
+      <VStack>
+      <Text
+          color={"#303030"}
+          fontSize={["50px", "50px", "60px", "80px", "95px"]}
+          fontFamily={"Playfair Display"}
+          fontWeight={"600"}
+          textAlign={"start"}>
+        300
+          </Text>
+          <Text
+          color={"#303030"}
+          fontSize={"16px"}
+          fontFamily={"Playfair Display"}
+          fontWeight={"300"}
+          textAlign={"center"}  w={"80%"}  >
+      Custom Projects Per Year
+          </Text>
+        
+      </VStack>
+
+    </HStack>
+  
+    <HStack justify={"center"} w={"100%"} h={"100%"} mt={"25px"}
+  gap={["25px", "100px", "200px", "350px", "450px"]} >
+      <VStack>
+      <Text
+          color={"#303030"}
+          fontSize={["50px", "50px", "60px", "80px", "95px"]}
+          fontFamily={"Playfair Display"}
+          fontWeight={"600"}
+          textAlign={"start"}>
+        25
+          </Text>
+          <Text
+          color={"#303030"}
+          fontSize={"16px"}
+          fontFamily={"Playfair Display"}
+          fontWeight={"300"}
+          textAlign={"center"}  w={"80%"}  >
+     Years of Industry Experience
+          </Text>
+        
+      </VStack>
+
+      <VStack>
+      <Text
+          color={"#303030"}
+          fontSize={["50px", "50px", "60px", "80px", "95px"]}
+          fontFamily={"Playfair Display"}
+          fontWeight={"600"}
+         textAlign={"center"}>
+      5
+          </Text>
+          <Text
+          color={"#303030"}
+          fontSize={"16px"}
+          fontFamily={"Playfair Display"}
+          fontWeight={"300"}
+          textAlign={"center"}  w={"80%"}  >
+     Offices Nationwide
+          </Text>
+        
+      </VStack>
+
+    </HStack>
+  
+  
+          
+    </>
   )
 }
 
